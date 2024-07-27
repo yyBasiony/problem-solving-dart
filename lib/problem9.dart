8390b2f8b7dbf0782df31d977b13fc5964d87464
Your Name <your_email@example.com>
2024-07-27 15:04:43 +0300

    p9

---
+++ lib/problem9.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+main(){
+  int? a;
+  int? b;
+  String input=stdin.readLineSync()!;
+  List<String>inputes=input.split(' ');
+   a=int.parse(inputes[0]);
+   b=int.parse(inputes[1]);
+
+  if(a>=b)
+    {
+      print("Yes");
+    }else{
+    print("No");
+
+  }
+
+}
